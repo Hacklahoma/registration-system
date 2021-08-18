2302cce024a6e6f0ef83b1e602c0935b29bd71a1
jacobbaber <jacob.baber@ou.edu>
2021-08-18 17:54:31 -0500

    Sign-up page complete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./pages/login";
+import Signup from "./pages/sign-up";
 
 function App() {
   return (
@@ -8,6 +9,11 @@ function App() {
       <Switch>
         <Route path="/login">
           <Login />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/sign-up">
+          <Signup />
         </Route>
       </Switch>
     </>
