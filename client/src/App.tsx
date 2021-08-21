2faaa3f29f65c3fcbed801cfa775d03b8ba3b7ee
jacob baber <jacob.baber@ou.edu>
2021-08-21 15:12:26 -0500

    Started admin page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Admin from "./pages/admin";
 import Confirmation from "./pages/confirmation";
 import Login from "./pages/login";
 import Signup from "./pages/sign-up";
@@ -20,6 +21,11 @@ function App() {
       <Switch>
         <Route path="/confirmation">
           <Confirmation />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/admin">
+          <Admin/>
         </Route>
       </Switch>
     </>
--- client/src/pages/admin.tsx
+++ client/src/pages/admin.tsx
@@ -1,7 +1,54 @@
 import React from "react";
+import MediaQuery from "react-responsive";
+import slack from "../assets/slack_logo.svg"
+import "../style/admin.scss"
+
+import Clouds from "../components/clouds";
+import trail from "../assets/paper-airplane.png"
 
 const Admin: React.FC = () => {
-  return <div className="admin"></div>;
+  return (
+  <div className="admin-login-page d-flex align-items-center justify-content-center">
+    <div className="center">
+
+    <MediaQuery minWidth={480}>
+    <img 
+      src={trail} 
+      className="trail"
+      style={{ top: "5vh", left: "0"}}
+      alt="plane"
+      />
+      <img 
+      src={trail} 
+      className="trail"
+      style={{ bottom: "0", right: "5vw"}}
+      alt="plane"
+      />
+      </MediaQuery>
+      <Clouds/>
+
+      <h1 className="hacklahoma">Hacklahoma</h1>
+        <div className="login-card admin-card">
+          <h6 className="card-title">Admin Login</h6>
+
+
+
+        <img src={slack} alt="" className="slack-logo" />
+
+      </div>
+    
+
+
+    
+
+
+
+      </div>
+  </div>
+
+  );
+
 };
+  
 
 export default Admin;
