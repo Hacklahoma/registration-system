1cbbe857482f1eb5be8787be0d80bc4bce2a604c
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2021-09-10 08:41:59 -0500

    Added stubs for Event endpoints

---
+++ server/app/Controllers/Http/EventsController.ts
@@ -0,0 +1,23 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class EventsController {
+    public async getAllEvents() {
+
+    }
+
+    public async getEvent() {
+        
+    }
+
+    public async createEvent() {
+        
+    }
+
+    public async editEvent() {
+        
+    }
+
+    public async deleteEvent() {
+        
+    }
+}
